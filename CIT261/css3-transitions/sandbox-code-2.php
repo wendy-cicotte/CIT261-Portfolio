ca2ce11727254ebf1aff05686c8a7a4443b9ff3b
Wendy Cicotte <wendycicotte@gmail.com>
2016-03-04 16:17:59 -0800

    12/12/15 - Completely re-did my CSS3 transition sandbox code 2 to incorporate a javascript trigger

--- CIT261/css3-transitions/sandbox-code-2.php
+++ CIT261/css3-transitions/sandbox-code-2.php
@@ -16,6 +16,20 @@ div#box1 {
 	background: #9DCEFF;
 	width: 400px;
 	height: 200px;
+        padding: 10px;
+        margin:200px;
+}
+#one{
+    margin-left:200px;
+    margin-top:25px;
+}
+#two{
+    margin-left: 275px;
+    margin-top: 25px;
+}
+#three{
+    margin-left:350px;
+    margin-top:25px;
 }
 </style>
 <script>
@@ -24,11 +38,19 @@ function changeBG(el,clr){
 	elem.style.transition = "background 1.0s linear 0s";
 	elem.style.background = clr;
 }
+function changeDimension(){
+    var elem = document.getElementById("box1");
+    elem.style.transition = "width 3s ease-in 0s";
+    elem.style.width = "600px";
+}
 </script>
 <body>
-<button onclick="changeBG('box1','#F0F')">Magenta</button>
-<button onclick="changeBG('box1','#0C0')">Green</button>
-<button onclick="changeBG('box1','#9DCEFF')">Original</button>
-<div id="box1">Content in box 1 ...</div>
+<button id="one" onclick="changeBG('box1','#F0F')">Magenta</button>
+<button id="two" onclick="changeBG('box1','#0C0')">Green</button>
+<button id="three" onclick="changeBG('box1','#9DCEFF')">Original</button>
+<div id="box1" onmouseover="changeDimension()">Click one of the buttons to fire off
+    </br> a JavaScript event and change the background of this box,
+    </br>or hover to see a change in dimension.
+</div>
 </body>
 </html>
